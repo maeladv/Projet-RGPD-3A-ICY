2a5314eb936de1c03291c08339c6059d3aaf6749
“Maeladv” <advissemael@gmail.com>
2025-11-25 15:33:23 +0100

    modif compose.yml

--- sql/init/init.sql
+++ sql/init/init.sql
@@ -1,22 +1,22 @@
-CREATE TABLE answers (
+CREATE TABLE IF NOT EXISTS answers (
     id SERIAL PRIMARY KEY,
 
-    nom VARCHAR(50) NOT NULL,
-    prenom VARCHAR(50) NOT NULL,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
 
     date_naissance DATE NOT NULL,
-    ville_naissance VARCHAR(50) NOT NULL,
+    ville_naissance VARCHAR(100) NOT NULL,
 
     niveau_diplome VARCHAR(100) NOT NULL,
 
-    mail VARCHAR(100) NOT NULL,
+    mail VARCHAR(255) NOT NULL,
 
-    adresse VARCHAR(100) NOT NULL,
-    complement_adresse VARCHAR(100),
-    code_postal VARCHAR(100) NOT NULL,
+    adresse VARCHAR(255) NOT NULL,
+    complement_adresse VARCHAR(255),
+    code_postal VARCHAR(20) NOT NULL,
     ville VARCHAR(100) NOT NULL,
     pays VARCHAR(100) NOT NULL,
 
-    num_secu_sociale VARCHAR(15) NOT NULL,
-    num_telephone VARCHAR(12) NOT NULL
+    num_secu_sociale VARCHAR(50) NOT NULL,
+    num_telephone VARCHAR(50) NOT NULL
 );
