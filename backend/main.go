cb125e9ff42498f3dcefad5c8df91d68addea84d
“Maeladv” <advissemael@gmail.com>
2025-11-28 16:03:51 +0100

    creation fichier routes.go + modif main

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,7 @@ package main
 import (
     "backend/auth"
     "backend/db"
-    "backend/handlers"
+	"backend/routes"
     "log"
     "net/http"
 )
@@ -17,13 +17,7 @@ func main() {
     }
     defer db.CloseDB()
 
-    http.HandleFunc("/api/forms", handlers.RequireJWT(database, handlers.GetAllForms(database)))
-    http.HandleFunc("/api/form", handlers.RequireJWT(database, handlers.GetForm(database)))
-    http.HandleFunc("/api/form/add", handlers.RequireJWT(database, handlers.AjoutForm(database)))
-
-    http.HandleFunc("/api/user/add", handlers.AjoutUser(database))       // inscription publique
-    http.HandleFunc("/api/login", handlers.LoginHandler(database))       // login pour obtenir JWT
-    http.HandleFunc("/api/users", handlers.RequireJWT(database, handlers.GetAllUsersHandler(database)))
+	routes.SetupRoutes(database)
 
     log.Println("[i] Serveur démarré sur le port 8080")
     log.Fatal(http.ListenAndServe(":8080", nil))
