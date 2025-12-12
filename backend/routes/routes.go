0978daf7373f2669273b99077d0ee47d8eac24af
“Maeladv” <advissemael@gmail.com>
2025-12-12 11:50:50 +0100

    modif routes

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -15,11 +15,11 @@ func SetupRoutes(database *sql.DB) {
     http.HandleFunc("/api/forms", middleware.RequireRole(database, []string{"rh", "admin"}, handlers.GetAllForms(database)))
     http.HandleFunc("/api/form", middleware.RequireRole(database, []string{"rh", "admin"}, handlers.GetForm(database)))
     http.HandleFunc("/api/form/add", middleware.RequireRole(database, []string{"rh", "admin"}, handlers.AjoutForm(database)))
-    http.HandleFunc("/api/form/delete", middleware.RequireRole(database, []string{"rh", "admin"}, handlers.SuppForm(database)))
-    http.HandleFunc("/api/form/modify", middleware.RequireRole(database, []string{"rh", "admin"}, handlers.ModifForm(database)))
+    
 
     // Routes Admin
-
+    http.HandleFunc("/api/form/delete", middleware.RequireRole(database, []string{"admin"}, handlers.SuppForm(database)))
+    http.HandleFunc("/api/form/modify", middleware.RequireRole(database, []string{"admin"}, handlers.ModifForm(database)))
     http.HandleFunc("/api/users", middleware.RequireRole(database, []string{"admin"}, handlers.GetAllUsersHandler(database)))
     http.HandleFunc("/admin", middleware.RequireRole(database, []string{"admin"}, func(w http.ResponseWriter, r *http.Request) {
         http.ServeFile(w, r, "/usr/share/nginx/html/pages/admin.html")
