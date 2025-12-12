1b6a33c8392e0bb0d2883c21fa5b1c39b37a82cf
“Maeladv” <advissemael@gmail.com>
2025-12-12 11:56:16 +0100

    fix: redirection quand on a pas le bon role

--- backend/middleware/role.go
+++ backend/middleware/role.go
@@ -10,6 +10,7 @@ func RequireRole(db *sql.DB, roles []string, next http.HandlerFunc) http.Handler
 	return RequireJWT(db, func(w http.ResponseWriter, r *http.Request) {
 		claims := GetClaims(r)
 		if claims == nil || !slices.Contains(roles, claims.Role) {
+            http.Redirect(w, r, "/login", http.StatusFound)
 			return
 		}
 		next(w, r)
