c197deb1020871bb950b8ce5701fc370eb5b47a8
“Maeladv” <advissemael@gmail.com>
2025-12-12 11:36:02 +0100

    fix: roles necessaires pour utliiser l'api

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -12,16 +12,15 @@ func SetupRoutes(database *sql.DB) {
 	http.HandleFunc("/rh", middleware.RequireRole(database, []string{"rh", "admin"}, func(w http.ResponseWriter, r *http.Request) {
         http.ServeFile(w, r, "/usr/share/nginx/html/pages/rh.html")
     }))
+    http.HandleFunc("/api/forms", middleware.RequireRole(database, []string{"rh", "admin"}, handlers.GetAllForms(database)))
+    http.HandleFunc("/api/form", middleware.RequireRole(database, []string{"rh", "admin"}, handlers.GetForm(database)))
+    http.HandleFunc("/api/form/add", middleware.RequireRole(database, []string{"rh", "admin"}, handlers.AjoutForm(database)))
+    http.HandleFunc("/api/form/delete", middleware.RequireRole(database, []string{"rh", "admin"}, handlers.SuppForm(database)))
+    http.HandleFunc("/api/form/modify", middleware.RequireRole(database, []string{"rh", "admin"}, handlers.ModifForm(database)))
 
     // Routes Admin
+    http.HandleFunc("/api/users", middleware.RequireRole(database, []string{"admin"}, handlers.GetAllUsersHandler(database)))
 
-	// Routes protégées (nécessitent JWT)
-    http.HandleFunc("/api/forms", middleware.RequireJWT(database, handlers.GetAllForms(database)))
-    http.HandleFunc("/api/form", middleware.RequireJWT(database, handlers.GetForm(database)))
-    http.HandleFunc("/api/form/add", middleware.RequireJWT(database, handlers.AjoutForm(database)))
-    http.HandleFunc("/api/form/delete", middleware.RequireJWT(database, handlers.SuppForm(database)))
-    http.HandleFunc("/api/form/modify", middleware.RequireJWT(database, handlers.ModifForm(database)))
-    http.HandleFunc("/api/users", middleware.RequireJWT(database, handlers.GetAllUsersHandler(database)))
 
     // Routes publiques (pas de JWT)
     http.HandleFunc("/api/user/add", handlers.AjoutUser(database))
